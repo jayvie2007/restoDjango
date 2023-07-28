bf4313c1a8f34c425acf5251f1f10fd419c56d26
Jayvie De Leon <123736031+jayvie2007@users.noreply.github.com>
2023-07-28 10:02:54 +0800

    added class-based views

--- restodjango/jresto/urls.py
+++ restodjango/jresto/urls.py
@@ -13,20 +13,20 @@ urlpatterns = [
 
 ###### API_TEMPLATES ######
 urlpatterns += [
-    path('api/product/food', views_api_templates.menu_get_food.as_view(), name="get_food"),
-    path('api/product/addfood', views_api_templates.menu_add_food.as_view(), name="add_food"),
-    path('api/product/editfood', views_api_templates.menu_edit_food.as_view(), name="edit_food"),
-    path('api/product/deletefood', views_api_templates.menu_delete_food.as_view(), name="delete_food"),
+    path('api/product/food', views_api_templates.GetFoodAPIView.as_view(), name="get_food"),
+    path('api/product/addfood', views_api_templates.PostFoodAPIView.as_view(), name="add_food"),
+    path('api/product/editfood', views_api_templates.PutFoodAPIView.as_view(), name="edit_food"),
+    path('api/product/deletefood', views_api_templates.DeleteFoodAPIView.as_view(), name="delete_food"),
     
-    path('api/product/drink', views_api_templates.menu_get_drink.as_view(), name="get_drink"),
-    path('api/product/adddrink', views_api_templates.menu_add_drink.as_view(), name="add_drink"),
-    path('api/product/editdrink', views_api_templates.menu_edit_drink.as_view(), name="edit_drink"),
-    path('api/product/deletedrink', views_api_templates.menu_delete_drink.as_view(), name="delete_drink"),
+    path('api/product/drink', views_api_templates.GetDrinkAPIView.as_view(), name="get_drink"),
+    path('api/product/adddrink', views_api_templates.PostDrinkAPIView.as_view(), name="add_drink"),
+    path('api/product/editdrink', views_api_templates.PutDrinkAPIView.as_view(), name="edit_drink"),
+    path('api/product/deletedrink', views_api_templates.DeleteDrinkAPIView.as_view(), name="delete_drink"),
     
-    path('api/product/side', views_api_templates.menu_get_side.as_view(), name="get_side"),
-    path('api/product/addside', views_api_templates.menu_add_side.as_view(), name="add_side"),
-    path('api/product/editside', views_api_templates.menu_edit_side.as_view(), name="edit_side"),
-    path('api/product/deleteside', views_api_templates.menu_delete_side.as_view(), name="delete_side"),
+    path('api/product/side', views_api_templates.GetSideAPIView.as_view(), name="get_side"),
+    path('api/product/addside', views_api_templates.PostSideAPIView.as_view(), name="add_side"),
+    path('api/product/editside', views_api_templates.PutSideAPIView.as_view(), name="edit_side"),
+    path('api/product/deleteside', views_api_templates.DeleteSideAPIView.as_view(), name="delete_side"),
 ]
 
 ###### AUTHENTICATION ######
--- restodjango/jresto/views/views_api_templates.py
+++ restodjango/jresto/views/views_api_templates.py
@@ -11,62 +11,80 @@ from jresto.utils import *
 from constants.status_code import *
 
 
-def menu_get_food(request):
-    foods = Food.objects.all()
-    food_serializers = FoodGetSerializer(foods, many=True)
-    return Response({"Food List": food_serializers.data})
+class GetFoodAPIView(APIView):
+    def get(self, request):
+        foods = Food.objects.all()
+        food_serializers = FoodGetSerializer(foods, many=True)
+        return Response({"Food List": food_serializers.data})
 
-def menu_add_food(self, request):
-    errors = {}
-    required_fields = ['name', 'description', 'price',]
+class PostFoodAPIView(APIView):
+    def post(self, request):
+        errors = {}
+        required_fields = ['name', 'description', 'price',]
 
-    for required_field in required_fields:
-        if required_field not in request.data:
-            errors[required_field] = field_required_error
-        if len(errors) != 0:
-            return Response(data={"status":bad_request, 'message':errors}, status=bad_request)
-        
-    product_name = request.data['name']
-    product_price = request.data['price']
-    product_description = request.data['description']
+        for required_field in required_fields:
+            if required_field not in request.data:
+                errors[required_field] = field_required_error
+            if len(errors) != 0:
+                return Response(data={"status":bad_request, 'message':errors}, status=bad_request)
+            
+        product_name = request.data['name']
+        product_price = request.data['price']
+        product_description = request.data['description']
 
-    try:
-        if 'name' in request.data and Food.objects.filter(name = product_name).count() != 0:
-            errors['name']=(f"The product {product_name} is already added!")
-            return Response(data={"message":errors}, status=bad_request)
-    except:
-        product_uid = generate_fooduid()
-        request.data._mutable=True
-        request.data['uid'] = product_uid
-        request.data._mutable=False
-        food_serializers = FoodGetSerializer(data=request.data)
-        if food_serializers.is_valid():
-            #food_serializers.save()
-            return Response(data={"status": created, "message": register_success, "Users":serializers.data})
-        return Response(food_serializers.errors, status=bad_request)
+        try:
+            if 'name' in request.data and Food.objects.filter(name = product_name).count() != 0:
+                errors['name']=(f"The product {product_name} is already added!")
+                return Response(data={"message":errors}, status=bad_request)
+        except:
+            product_uid = generate_fooduid()
+            request.data._mutable=True
+            request.data['uid'] = product_uid
+            request.data._mutable=False
+            food_serializers = FoodGetSerializer(data=request.data)
+            if food_serializers.is_valid():
+                #food_serializers.save()
+                return Response(data={"status": created, "message": register_success, "Users":food_serializers.data})
+            return Response(food_serializers.errors, status=bad_request)
 
-def menu_edit_food(self, request):
-    pass
-def menu_delete_food(self, request):
-    pass
+class PutFoodAPIView(APIView):
+    def put(self, request):
+        pass
 
-def menu_get_drink(self, request):
-    drinks = Drink.objects.all()
-    drink_serializers = DrinkGetSerializer(drinks, many=True)
-    return Response({"Food List": drink_serializers.data})
+class DeleteFoodAPIView(APIView):    
+    def delete(self, request):
+        pass
 
-def menu_add_drink(self, request):
-    pass
-def menu_edit_drink(self, request):
-    pass
-def menu_delete_drink(self, request):
-    pass
+class GetDrinkAPIView(APIView):
+    def get(self, request):
+        drinks = Drink.objects.all()
+        drink_serializers = DrinkGetSerializer(drinks, many=True)
+        return Response({"Food List": drink_serializers.data})
 
-def menu_get_side(self, request):
-    pass
-def menu_add_side(self, request):
-    pass
-def menu_edit_side(self, request):
-    pass
-def menu_delete_side(self, request):
-    pass
+class PostDrinkAPIView(APIView):
+    def menu_add_drink(self, request):
+        pass
+
+class PutDrinkAPIView(APIView):    
+    def menu_edit_drink(self, request):
+        pass
+    
+class DeleteDrinkAPIView(APIView):    
+    def menu_delete_drink(self, request):
+        pass
+
+class GetSideAPIView(APIView):
+    def menu_get_side(self, request):
+        pass
+
+class PostSideAPIView(APIView):    
+    def menu_add_side(self, request):
+        pass
+
+class PutSideAPIView(APIView):
+    def menu_edit_side(self, request):
+        pass
+    
+class DeleteSideAPIView(APIView):  
+    def menu_delete_side(self, request):
+        pass
