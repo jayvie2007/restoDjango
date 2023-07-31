2afc75f28a69d9281c4291a877734961b08f53b4
Jayvie De Leon <123736031+jayvie2007@users.noreply.github.com>
2023-07-31 10:01:55 +0800

    docker is now functionable

--- restodjango/jresto/views/views_authentication.py
+++ restodjango/jresto/views/views_authentication.py
@@ -1,4 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.hashers import make_password
+
+from jresto.models import CustomAdmin, CustomerDetails
+from jresto.utils import *
+
+from constants.status_code import *
+
 
 def register(request):
     return render(request, 'admin/authentication/register.html')
--- restodjango/jresto/views/views_templates.py
+++ restodjango/jresto/views/views_templates.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'dashboard/dashboard.html')
+    return render(request, 'admin/authentication/register.html')
 
