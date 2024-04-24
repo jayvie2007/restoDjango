4bb107a6bbb04ad9869942682ddad1600131aceb
Jayvie De Leon <123736031+jayvie2007@users.noreply.github.com>
2024-04-24 17:20:31 +0800

    add: TransactionHistory Model

---
+++ restodjango/jresto/choices.py
@@ -0,0 +1,29 @@
+GENDER_CHOICES = (
+    ('Male', 'Male'),
+    ('Female', 'Female'),
+    ('Other', 'Other'),
+)
+
+
+PERMISSION_CHOICES = (
+    ('Admin', 'Admin'),
+    ('Customer', 'Customer'),
+)
+
+
+PRODUCT_CHOICES = (
+    ('Meal', 'Meal'),
+    ('Drink', 'Drink'),
+    ('Side', 'Side'),
+)
+
+
+PROCESS_TYPE_CHOICES = (
+    ('Cancelled', 'Cancelled'),
+    ('Pending', 'Pending'),
+    ('Completed', 'Completed'),
+)
+
+ALL_TRANSACTION_TYPE = (
+    ('', '')
+)
--- restodjango/jresto/models.py
+++ restodjango/jresto/models.py
@@ -2,39 +2,22 @@ from django.db import models
 from django.core.validators import RegexValidator
 from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.conf import settings
+from django.utils import timezone
 
 from datetime import date
 
+from jresto.choices import *
 from jresto.utils import *
 
 import os
-# Create your models here.
 
-gender_choices = (
-    ('Male', 'Male'),
-    ('Female', 'Female'),
-    ('Other', 'Other'),
-)
-permission_choices = (
-    ('Admin', 'Admin'),
-    ('Customer', 'Customer'),
-)
-product_choices = (
-    ('Meal', 'Meal'),
-    ('Drink', 'Drink'),
-    ('Side', 'Side'),
-)
-process_type = (
-    ('Cancelled', 'Cancelled'),
-    ('Pending', 'Pending'),
-    ('Completed', 'Completed'),
-)
 
 contact_number_regex = r'^(\+[0-9]{1,12})|[0-9]{1,11}$'
 contact_number_validator = RegexValidator(
     regex=contact_number_regex,
     message='Contact number must start with + and include only digits.'
 )
+
 
 class CustomerFeedback(models.Model):
     name = models.CharField(max_length=25)
@@ -48,7 +31,7 @@ class CustomerFeedback(models.Model):
 
 class Product(models.Model):
     product_id = models.CharField(max_length=16, default="")
-    product_type = models.CharField(max_length=15, choices=product_choices, default="")
+    product_type = models.CharField(max_length=15, choices=PRODUCT_CHOICES, default="")
     name = models.CharField(max_length=25)
     picture = models.ImageField(blank=True, null=True)
     price = models.IntegerField(default=0)
@@ -74,15 +57,16 @@ class Product(models.Model):
             self.picture = None
             self.save(update_fields=['picture'])
     
+
 class CustomUser (AbstractUser):
     uid = models.CharField(max_length=20, editable=False)
     middle_name = models.CharField(max_length=50, default="", blank=True)
-    gender = models.CharField(max_length=6, choices=gender_choices, blank=True)
+    gender = models.CharField(max_length=6, choices=GENDER_CHOICES, blank=True)
     contact_number = models.CharField(max_length=12, validators=[contact_number_validator], blank=True)
     groups = models.ManyToManyField(Group, blank=True, related_name='CustomerDetails')
     user_permissions = models.ManyToManyField(Permission, blank=True, related_name='CustomerDetails')
     save_password = models.CharField(max_length=20, default="", blank=True)
-    user_level = models.CharField(max_length=20, choices=permission_choices)
+    user_level = models.CharField(max_length=20, choices=PERMISSION_CHOICES)
     date_created = models.DateField(auto_now_add=True)
     date_updated = models.DateField(auto_now=True)
 
@@ -102,6 +86,7 @@ class CustomUser (AbstractUser):
     class Meta:
         verbose_name = "User"
 
+
 class Customer(models.Model):
     customer = models.OneToOneField(CustomUser,on_delete=models.CASCADE,primary_key=True,related_name='wallet')
     cash = models.IntegerField(default=0)
@@ -114,7 +99,7 @@ class Order(models.Model):
     customer = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True, blank=True)
     transaction_id = models.CharField(max_length=100, null=True)
     total_bill = models.IntegerField(null=True, default=0)
-    status = models.CharField(choices=process_type, max_length=20, default="Pending")
+    status = models.CharField(choices=PROCESS_TYPE_CHOICES, max_length=20, default="Pending")
     date_created = models.DateField(auto_now_add=True)
     date_completed = models.DateField(null=True)
     
@@ -127,6 +112,7 @@ class Order(models.Model):
 
     def __str__(self):
         return f"{self.id}. {self.customer.first_name} {self.customer.last_name}"
+
 
 class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, blank=True)
@@ -143,6 +129,7 @@ class OrderItem(models.Model):
     def __str__(self):
         return f"{self.order.id}. {self.order.customer}, Product: {self.product.name}, Quantity: {self.quantity}"
 
+
 class DeliveryInfo(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
@@ -157,3 +144,18 @@ class DeliveryInfo(models.Model):
     
     def __str__(self):
         return f"Delivery: {self.id}. {self.first_name} {self.last_name}"
+
+
+
+class TransactionHistory(models.Model):
+    customer_id = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
+    transaction_id = models.IntegerField(null=True, blank=True)
+    transaction_type = models.CharField(choices=ALL_TRANSACTION_TYPE, max_length=50, blank=False, null=False)
+    amount = models.DecimalField(max_digits=100, default=0, decimal_places=0, null=True, blank=True)
+    before_amount = models.DecimalField(max_digits=100, default=0, decimal_places=0, null=True, blank=True)
+    final_amount = models.DecimalField(max_digits=100, default=0, decimal_places=0, null=True, blank=True)
+    date_created = models.DateTimeField(default=timezone.now)
+    date_updated = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.id} {self.transaction_type} {self.user_id}'
